2ff7074d7e3a5275267938ecebe10f11f0d84e45
Saeed Ahmed <saeed.cs17@iba-suk.edu.pk>
2022-04-04 16:38:03 +0500

    khan

--- NewBlazorProject/Server/Services/AdminService.cs
+++ NewBlazorProject/Server/Services/AdminService.cs
@@ -14,6 +14,7 @@ namespace NewBlazorProject.Server.Services
         }
 
         // commit added by saeed
+        // commit added by ali
        async Task<IEnumerable<MD_Address>>  IAdminInterface.AddAddress(MD_Address mD_Address)
         {
             var result = await _context.MD_Addresses.AddAsync(mD_Address);
